bafb1107927bf98deeec9c953f81764daa7af686
ashwithans <ashwithans@gmail.com>
2023-11-29 15:52:49 +0530

    Assignment_day18

---
+++ ConAppDay18Assignment/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConAppDay18Assignment
+{
+    public class Game
+    {
+        public int EnergyLevel { get; private set; }
+        public int WinningProbability { get; private set; }
+        private Random random;
+
+        public Game()
+        {
+            EnergyLevel = 1;
+            WinningProbability = 100;
+            random = new Random();
+        }
+
+        public void Spin()
+        {
+            int spinResult = random.Next(1, 11); // Simulating a random spin result between 1 and 10
+            switch (spinResult)
+            {
+                case 1:
+                    EnergyLevel += 1;
+                    WinningProbability += 10;
+                    break;
+                case 2:
+                    EnergyLevel += 2;
+                    WinningProbability += 20;
+                    break;
+                case 3:
+                    EnergyLevel -= 3;
+                    WinningProbability -= 30;
+                    break;
+                case 4:
+                    EnergyLevel += 4;
+                    WinningProbability += 40;
+                    break;
+                case 5:
+                    EnergyLevel += 5;
+                    WinningProbability += 50;
+                    break;
+                case 6:
+                    EnergyLevel -= 1;
+                    WinningProbability -= 60;
+                    break;
+                case 7:
+                    EnergyLevel += 1;
+                    WinningProbability += 70;
+                    break;
+                case 8:
+                    EnergyLevel -= 2;
+                    WinningProbability -= 20;
+                    break;
+                case 9:
+                    EnergyLevel -= 3;
+                    WinningProbability -= 30;
+                    break;
+                case 10:
+                    EnergyLevel += 10;
+                    WinningProbability += 100;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ConAppDay18Assignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDay18Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            do
+            {
+                Console.WriteLine("Enter Your Name");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Your Lucky Number from 1-10");
+                int num = int.Parse(Console.ReadLine());
+
+                Game game = new Game();
+
+                for (int i = 0; i < num; i++)
+                {
+                    game.Spin();
+                }
+
+                if (game.EnergyLevel >= 4 && game.WinningProbability > 60)
+                {
+                    Console.WriteLine("Winner");
+                }
+                else if (game.EnergyLevel >= 1 && game.WinningProbability > 50)
+                {
+                    Console.WriteLine("Runner Up");
+                }
+                else
+                {
+                    Console.WriteLine("Looser");
+                }
+
+                Console.WriteLine("If you want to continue Then press y");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
